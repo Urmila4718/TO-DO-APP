3a7f6c50167a9f649840176c18cf11acfe4f4fcb
Urmila4718 <79640277+Urmila4718@users.noreply.github.com>
2023-11-10 14:46:51 +0530

    Update forms.py

--- todo/forms.py
+++ todo/forms.py
@@ -6,3 +6,6 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = "__all__"
+        widgets = {
+            'details': forms.Textarea(attrs={'cols': 30, 'rows': 10}),
+        }
