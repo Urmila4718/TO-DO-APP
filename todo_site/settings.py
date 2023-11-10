883a03fb80a2f3792b3b983d24d11ca15a7cab41
Urmila4718 <79640277+Urmila4718@users.noreply.github.com>
2023-11-10 13:11:15 +0530

    Update settings.py

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -26,7 +26,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-*2&f2afg%5k#=lit0&899+iyx=au!g2j^p859-!mfs_ik6h!-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
